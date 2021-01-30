c8df4a83890f173ad3e8f8b7bb2eaf7b1d66a801
fingertwiddler <fingertwiddler@gmail.com>
2021-01-30 14:51:00 -0500

    x

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 import RSS from "https://jspm.dev/rss"
+import marked from "https://jspm.dev/marked"
 export default async function (items, config, offbase) {
   // Do RSS only when there's custom BASE URL
   // Instantiate RSS
   let feed = new RSS()
   let feedItems = items.slice(0, config.FEED.CHUNK)
   feedItems.forEach((item) => {
+    let html;
+    if (config.FEED.BASE.length === 0) {
+      html = marked(item.content, { baseUrl: "../" }) 
+    } else {
+      html = marked(item.content, { baseUrl: config.FEED.BASE }) 
+    }
     feed.item({
       title: item.data.title,
-      description: item.html,
+      description: html,
       url: `${config.FEED.BASE}${item.key}`,
       date: item.data.updated
     })
