2497ad58ef8564a80ff3eb21c03b123d236d5ae2
fingertwiddler <fingertwiddler@gmail.com>
2021-01-30 19:48:51 -0500

    x

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ export default async function (items, config, offbase) {
   // Do RSS only when there's custom BASE URL
   // Instantiate RSS
   let feed = new RSS()
-  let feedItems = items.slice(0, config.FEED.CHUNK)
+  let feedItems = items.slice(0, config.settings.FEED.CHUNK)
   feedItems.forEach((item) => {
     let html;
-    if (config.FEED.BASE.length === 0) {
+    if (config.settings.FEED.BASE.length === 0) {
       html = marked(item.content, { baseUrl: "../" }) 
     } else {
-      html = marked(item.content, { baseUrl: config.FEED.BASE }) 
+      html = marked(item.content, { baseUrl: config.settings.FEED.BASE }) 
     }
     feed.item({
       title: item.data.title,
       description: html,
-      url: `${config.FEED.BASE}${item.key}`,
+      url: `${config.settings.FEED.BASE}${item.key}`,
       date: item.data.updated
     })
   })
   debugger;
   let xml = feed.xml({ indent: true });
-  await offbase.fs.promises.writeFile(`${config.DEST}/rss.xml`, xml)
+  await offbase.fs.promises.writeFile(`${config.settings.DEST}/rss.xml`, xml)
 }
